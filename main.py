d8c135c9c434478d7d00df220b69776463d8fb57
lee-janice <janicel375@gmail.com>
2023-05-08 16:13:18 -0400

    add endpoint for getting a paper by keyword

--- main.py
+++ main.py
@@ -31,16 +31,24 @@ def paper_by_ssid(request, semantic_scholar_id, fields):
     response = requests.get(f"{SEMANTIC_SCHOLAR_BASE_URL}{PAPER_ENDPOINT}{semantic_scholar_id}/?fields={fields}")
     return response.content
 
+@route("/api/paper/keyword:<keyword>/fields=<fields>", methods = ["GET"])
+def paper_by_keyword(request, keyword, fields): 
+    setHeader(request,'application/json')
+    print(f"{SEMANTIC_SCHOLAR_BASE_URL}{PAPER_ENDPOINT}search?query={keyword}&fields={fields}")
+    response = requests.get(f"{SEMANTIC_SCHOLAR_BASE_URL}{PAPER_ENDPOINT}search?query={keyword}&fields={fields}")
+    return response.content
+
 @route("/api/references/SSID:<semantic_scholar_id>/fields=<fields>", methods = ["GET"])
 def references_by_ssid(request, semantic_scholar_id, fields):
     setHeader(request,'application/json')
     response = requests.get(f"{SEMANTIC_SCHOLAR_BASE_URL}{PAPER_ENDPOINT}{semantic_scholar_id}/{REFERENCES_BRANCH}?fields={fields}")
     return response.content
 
-@route("/api/citations/SSID:<semantic_scholar_id>/fields=<fields>", methods = ["GET"])
-def citations_by_ssid(request, semantic_scholar_id, fields):
+# adding the limit parameter here so that we can get more influential citations (instead of only the most recent ones)
+@route("/api/citations/SSID:<semantic_scholar_id>/fields=<fields>&limit=<limit>", methods = ["GET"])
+def citations_by_ssid(request, semantic_scholar_id, fields, limit):
     setHeader(request,'application/json')
-    response = requests.get(f"{SEMANTIC_SCHOLAR_BASE_URL}{PAPER_ENDPOINT}{semantic_scholar_id}/{CITATIONS_BRANCH}?fields={fields}")
+    response = requests.get(f"{SEMANTIC_SCHOLAR_BASE_URL}{PAPER_ENDPOINT}{semantic_scholar_id}/{CITATIONS_BRANCH}?fields={fields}&limit={limit}")
     return response.content
 
 port = int(os.environ.get("PORT", 8080))
